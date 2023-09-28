b83af03c48329292a81946bd8b2a182dcdf1bfe5
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-09-28 14:48:45 +0200

    Commit

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -3,7 +3,7 @@ import Currency from './3-currency';
 export default class Pricing {
   constructor(amount, currency) {
     this._amount = amount;
-    this._currency = currency;
+    this._currency = new Currency(currency.code, currency.name);
   }
 
   get amount() {
