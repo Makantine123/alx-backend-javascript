ee996e81f3e24ec28e8940961cd58049bae39329
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-09-28 18:59:51 +0200

    Commit

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,12 +1,13 @@
 export default class Building {
   constructor(sqrt) {
-    if (this.constructor !== Building && this.evacuationWarningMessage !== 'function') {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
-    }
     this._sqrt = sqrt;
   }
 
   get sqrt() {
     return this._sqrt;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error('Class extendingBuilding must override evacuationWarningMessage');
   }
 }
