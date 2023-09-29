19b331e57b527678ae1cdb3a220f6ccdd94eb7e7
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-09-29 12:24:03 +0200

    Commit

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,12 +1,17 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, filename) {
-  return Promise
-    .allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
-    .then((result) => result.map((item) => ({
-      status: item.status,
-      value: item.status === 'fulfiled' ? item.value : String(item.reason),
-    })))
-    .catch(() => []);
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const res = [];
+  try {
+    const user = await signUpUser(firstName, lastName);
+    res.push({ status: 'fulfilled', value: user });
+    await uploadPhoto(fileName);
+  } catch (err) {
+    res.push({
+      status: 'rejected',
+      value: `Error: ${fileName} cannot be processed`,
+    });
+  }
+  return res;
 }
