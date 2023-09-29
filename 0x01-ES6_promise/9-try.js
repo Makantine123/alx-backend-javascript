ed2c98b4e613767442b2e0e0555869a8e784c0b8
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-09-29 13:34:01 +0200

    Commit

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -5,7 +5,7 @@ export default function guardrail(mathFunction) {
     const result = mathFunction();
     queue.push(result);
   } catch (error) {
-    queue.push('Error: ${error.message');
+    queue.push(error.toString());
   } finally {
     queue.push('Guardrail was processed');
   }
