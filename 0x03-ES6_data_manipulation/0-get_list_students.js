cf4ea6e9f9bbb8e3340f17dda57e6a0705517e25
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-10-02 19:36:40 +0200

    Commit

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++
@@ -1,8 +0,0 @@
-export default function getListStudents() {
-  const students = [
-    {id: 1, firstName: 'Guillaume', location: 'San Francisco'},
-    {id: 1, firstName: 'James', location: 'Columbia'},
-    {id: 1, firstName: 'Serena', location: 'San Francisco'},
-  ]
-  return students;
-}
