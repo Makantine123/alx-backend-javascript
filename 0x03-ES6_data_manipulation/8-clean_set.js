59b40ef1177a44a208a7df90de439dd9c7a0c731
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-10-03 14:20:14 +0200

    Commit

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 export default function cleanSet(set, startString) {
   const filterdArray = Array.from(set).filter((item) => item.startsWith(startString));
   const cleanArray = filterdArray.map((value) => value.substring(startString.length));
-  if (cleanArray.length > 0)
+  if (filterdArray.length > 0 && startString !== '')
     return cleanArray.join('-');
   return '';
 }
