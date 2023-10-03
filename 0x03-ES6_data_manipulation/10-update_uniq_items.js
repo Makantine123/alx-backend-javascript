b35430f009cdb810539c610a91a7a231c499d640
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-10-03 15:44:02 +0200

    Commit

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,7 @@
 export default function updateUniqueItems(myMap) {
+  if (!(myMap instanceof Map)) {
+    throw Error('Cannot process');
+  }
   myMap.set('Pasta', 100);
   myMap.set('Rice', 100);
   return myMap;
