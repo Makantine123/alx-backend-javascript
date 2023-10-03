f42af1a3c4dd6cd4f5bc5ca48f5a35999fc76d37
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-10-03 19:07:55 +0200

    Commit

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,10 +1,10 @@
 export default function createIntTypedArray(length, position, value) {
   if (position < 0 || position >= length) {
-    throw Error('Position outside range');
+    throw new Error('Position outside range');
   }
   const buffer = new ArrayBuffer(length);
   const int8view = new Int8Array(buffer);
 
   int8view[position] = value;
-  return int8view;
+  return new DataView(buffer);
 }
