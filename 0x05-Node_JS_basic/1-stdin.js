187d8d3955cf2f657aaf8eaeab1ce452954d4fc5
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-11-22 01:07:21 +0200

    Commit

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,15 +1,15 @@
 // Displays message followed by new line
 
-const greeting = 'Welcome to Holberton School, what is your name?\n';
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-const readline = require('readline');
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
 
-const r1 = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
 });
 
-r1.question(greeting, (callback) => {
-  console.log(`Your name is: ${callback}`);
-  r1.close();
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
