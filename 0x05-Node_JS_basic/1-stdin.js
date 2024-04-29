d95443972f624eaf2e5fcd82b4976afe81e64f45
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2024-04-30 01:42:19 +0200

    Commit

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
 // Displays message followed by new line
 
-process.stdout.write("Welcome to Holberton School, what is your name?\n")
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
 process.stdin.on('readable', () => {
   var chunk = process.stdin.read();
