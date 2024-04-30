4b32a9c76aeeaef6bbf3fb97d2d4686c25924dea
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2024-05-01 00:55:17 +0200

    Commit

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,59 @@
+/* Script creates HTTP server using Nodes HHTP module */
+
+const fs = require('fs');
+
+function countStudents(param) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(param, 'utf-8', (err, data) => {
+      if (err) {
+        reject(Error('Cannot load the database'));
+        return;
+      }
+
+      let lines = data.trim().split('\n');
+      const numberStudents = lines.length - 1;
+
+      const msg = [`Number of students: ${numberStudents}`];
+      console.log(msg[0]);
+
+      lines = lines.map((item) => item.split(','));
+      const groupedLines = {};
+      for (const i in lines) {
+        if (i !== 0) {
+          if (!groupedLines[lines[i][3]]) {
+            groupedLines[lines[i][3]] = [];
+          }
+          groupedLines[lines[i][3]].push(lines[i][0]);
+        }
+      }
+      delete groupedLines.field;
+
+      for (const key in groupedLines) {
+        if (key !== 0) {
+          const firstnames = groupedLines[key].join(' ,');
+          const msg2 = `Number of students in ${key}: ${groupedLines[key].length}. List: ${firstnames}`;
+          msg.push(msg2);
+          console.log(msg2);
+        }
+      }
+      resolve(msg);
+    });
+  });
+}
+
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    const param = process.argv[2];
+    countStudents(param);
+  }
+});
+
+app.listen(1245);
+
+module.exports = app;
