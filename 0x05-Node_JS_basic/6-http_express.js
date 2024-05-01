a89efa81a2cf136208f29addc8c505104c43b9ab
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2024-05-01 16:37:52 +0200

    Commit

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -9,6 +9,20 @@ app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
+app.use((req, res) => {
+  res.status(404).send(
+`<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Error</title>
+</head>
+<body>
+<pre>Cannot GET ${req.url}</pre>
+</body>
+</html>`);
+});
+
 app.listen(port, () => {
 });
 
