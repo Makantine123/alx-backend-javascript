7b8da6842b7a6f5e43c9dbce5014828fdb77364b
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2024-05-01 16:25:26 +0200

    Commit

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,14 @@
+/* Script creates HTTP server using Nodes HHTP module */
+
+const express = require('express');
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(port, () => {
+});
+
+module.exports = app;
