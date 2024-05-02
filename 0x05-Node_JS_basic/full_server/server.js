bb0505acd31e2dd15153bbc721f9327e90bdeb57
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2024-05-02 03:19:27 +0200

    Commit

--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -7,7 +7,9 @@ const express = require('express');
 const app = express();
 const port = 1245;
 
-app.use(AppRoutes);
+app.use('/', AppRoutes);
+app.use('/students', AppRoutes);
+app.use('/students/:major', AppRoutes);
 app.listen(port);
 
 export default app;
