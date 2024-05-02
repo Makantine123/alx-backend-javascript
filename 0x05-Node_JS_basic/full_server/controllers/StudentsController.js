69f196ae076a76c1e685081693d8dee2c66944b1
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2024-05-02 20:24:40 +0200

    Commit

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -4,12 +4,11 @@ const readDatabase = require('../utils');
 
 class StudentsController {
   static getAllStudents(request, response) {
-    response.status = 200;
-
-    response.write('This is the list of our students\n');
     readDatabase(process.argv[2].toString())
       .then((grouplines) => {
         const keys = Object.keys(grouplines).sort();
+        response.status(200);
+        response.write('This is the list of our students\n');
         for (let k = 0; k < keys.length; k += 1) {
           response.write(`Number of students in ${keys[k]}: ${grouplines[keys[k]].length}. List: ${grouplines[keys[k]].join(', ')}`);
           if (k + 1 < keys.length) {
