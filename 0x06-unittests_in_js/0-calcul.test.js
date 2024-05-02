9e95ed1484994372fef7923f162506a19bbf3e0b
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2024-05-02 22:09:05 +0200

    Commit

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -5,10 +5,23 @@ const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', function () {
   it('should return the sum of twon rounded numbers', () => {
-    assert.strictEqual(calculateNumber(1, 3), 4);
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.2, 0.7), 2);
-    assert.strictEqual(calculateNumber(1, 5.5), 7);
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
+
+  it('should return the sum of two rounded numbers', () => {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+  });
+
+  it('should return the sum of two rounded numbers', () => {
+    assert.strictEqual(calculateNumber(1.2, 0.7), 2);
+  });
+
+  it('should return the sum of two rounded numbers', () => {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
+
+  it('should return the sum of two rounded numbers', () => {
+    assert.strictEqual(calculateNumber(1, 5.5), 7);
+  });
+
 });
