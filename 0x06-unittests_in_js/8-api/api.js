a164f61a2948c713ab8a147868c775ec40e88aae
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2024-05-03 19:54:44 +0200

    Commit

---
+++ 0x06-unittests_in_js/8-api/api.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+
+const app = express();
+const port = 7865;
+
+app.get('/', (req, res) => {
+  res.end('Welcome to the payment system');
+});
+
+const server = app.listen(port, () => {
+  console.log('API available on localhost port 7865');
+});
+
+module.exports = { app, server};
---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,30 @@
+const assert = require('assert');
+const request = require('request');
+const { app, server } = require('./api.js');
+
+const baseURL = 'http://localhost:7865';
+
+describe('Index page', () => {
+  // Testing correct status code
+  it('Correct status code?', (done) => {
+    request.get(baseURL, (error, response, body) => {
+      assert.strictEqual(response.statusCode, 200);
+      done();
+    });
+  });
+
+  // Testing correct result
+  it('Correct result?', (done) => {
+    request.get(baseURL, (error, response, body) => {
+      assert.strictEqual(body, 'Welcome to the payment system');
+      done();
+    });
+  });
+
+  // Other tests can be added here
+
+  // Testing if the server is running
+  it('Server', () => {
+    assert.strictEqual(server.listening, true);
+  });
+});
