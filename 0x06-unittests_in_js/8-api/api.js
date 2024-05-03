527f6fb5cc9cf589ba679e5030b2ffe654f53797
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2024-05-03 20:04:08 +0200

    Commit

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -5,11 +5,11 @@ const app = express();
 const port = 7865;
 
 app.get('/', (req, res) => {
-  res.end('Welcome to the payment system');
+  res.send('Welcome to the payment system');
 });
 
 const server = app.listen(port, () => {
   console.log('API available on localhost port 7865');
 });
 
-module.exports = { app, server};
+module.exports = { app, server };
